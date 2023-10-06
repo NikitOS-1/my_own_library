f4aca1c10103676dad8de827e27b02bb8bc9e675
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-06 17:23:36 +0300

    add utils uniqueId

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { useState } from "react";
+import uniqueId from "./utils/generateId";
 
 function App() {
-  return <div className="App"></div>;
+  const [id, setId] = useState(0);
+
+  const updateId = () => {
+    setId(uniqueId);
+  };
+  return (
+    <div className="App">
+      <h1>{id}</h1>
+      <button onClick={updateId}>get new unique ID</button>
+    </div>
+  );
 }
 
 export default App;
