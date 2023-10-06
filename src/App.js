b232ee0efaca9553e56f2aa7fa21876b227c3080
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-06 17:29:53 +0300

    ADD UTILS convertToSlug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import uniqueId from "./utils/generateId";
+import convertToSlug from "./utils/convertSlug";
 
 function App() {
   const [id, setId] = useState(0);
+  const [text, setText] = useState("Hello");
 
   const updateId = () => {
     setId(uniqueId);
+    setText(convertToSlug(text));
   };
 
   return (
     <div className="App">
+      <h1>{text}</h1>
       <h1>{id}</h1>
+      <input onChange={(e) => setText(e.target.value)} />
       <button onClick={updateId}>get new unique ID</button>
     </div>
   );
