30cbb321328c5804d312de86e04df8f3de48466b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-09 21:59:55 +0300

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,19 +4,19 @@ const Home = () => {
   return (
     <div>
       <Grid container spacing={2}>
-        {/* <Grid item xs={12}>
-          <div>xs=12</div>
-        </Grid> */}
-        <Grid item xs={8}>
+        <Grid item xs={11} border={"1px solid black"}>
+          <div>xs=13</div>
+        </Grid>
+        <Grid item xs={8} border={"1px solid black"}>
           <div>xs=8</div>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} border={"1px solid black"}>
           <div>xs=4</div>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} border={"1px solid black"}>
           <div>xs=2</div>
         </Grid>
-        <Grid item xs={10}>
+        <Grid item xs={10} border={"1px solid black"}>
           <div>xs=10</div>
         </Grid>
       </Grid>
