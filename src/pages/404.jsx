c6dd2f4ffcf98af259eeb2cd59037653c9a011c0
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-10 16:53:08 +0300

    add clsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,20 @@
 const { useTheme } = require("@emotion/react");
+const { default: FlexBox } = require("components/FlexBox");
 
 const ErrorPage = () => {
   const theme = useTheme();
+  <FlexBox
+    p={4}
+    height="100%"
+    alignItems="center"
+    flexDirection="column"
+    justifyContent="center">
+    <Box maxWidth={350}>
+      <img
+        src="/static/illustration/error-page.svg"
+        width="100%"
+        alt="Error 404"
+      />
+    </Box>
+  </FlexBox>;
 };
