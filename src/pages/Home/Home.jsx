af7aac965eccda2796f10bdb373e8af8b4ce438c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-10 20:52:10 +0300

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,3 @@
-// import { Grid } from "@mui/material";
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Grid container spacing={2}>
-//         <Grid item xs={12} border={"1px solid black"}>
-//           <div>xs=12</div>
-//         </Grid>
-//         <Grid item xs={8} border={"1px solid black"}>
-//           <div>xs=8</div>
-//         </Grid>
-//         <Grid item xs={4} border={"1px solid black"}>
-//           <div>xs=4</div>
-//         </Grid>
-//         <Grid item xs={2} border={"1px solid black"}>
-//           <div>xs=2</div>
-//         </Grid>
-//         <Grid item xs={10} border={"1px solid black"}>
-//           <div>xs=10</div>
-//         </Grid>
-//       </Grid>
-//     </div>
-//   );
-// };
-// export default Home;
-
 import { Box, FormControlLabel, Radio, RadioGroup } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { SettingsContext } from "contexts/SettingsContext";
