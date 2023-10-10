15cff7f4115b19f777e6fe13bda18d136de60e60
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-10 21:28:54 +0300

    ref

--- src/redux/themeReducer.js
+++ src/redux/themeReducer.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { THEMES } from "constants";
+import useLocalStorage from "hooks/useLocalStorage";
 
 const initialState = {
   theme: THEMES.LIGHT,
@@ -9,8 +10,12 @@ const initialState = {
 export const themeSlice = createSlice({
   name: "theme",
   initialState,
-  reducers: {},
+  reducers: {
+    updateSettings: (state, action) => {
+      useLocalStorage("themeSettings", initialState);
+    },
+  },
 });
 
-export const {} = themeSlice.actions;
+export const { updateSettings } = themeSlice.actions;
 export default themeSlice.reducer;
