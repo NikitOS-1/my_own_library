8c0c5ce8286af8c6f40067057386fc653698cb2e
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-10 21:37:11 +0300

    del

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from "./themeReducer";
 
 export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
+  reducer: {},
 });
