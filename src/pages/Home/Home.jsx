feb8c2dc1faa1e8146449a01e09b69f55e3c5fbc
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-10 19:13:38 +0300

    ref

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div>
       <Grid container spacing={2}>
-        <Grid item xs={11} border={"1px solid black"}>
-          <div>xs=13</div>
+        <Grid item xs={12} border={"1px solid black"}>
+          <div>xs=12</div>
         </Grid>
         <Grid item xs={8} border={"1px solid black"}>
           <div>xs=8</div>
