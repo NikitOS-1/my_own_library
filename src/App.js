ec2d8dd5c9d4eb50a4a6b010026f4caf3a77083a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-10 15:53:11 +0300

    add dep and theme style

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import {
   StyledEngineProvider,
   ThemeProvider,
 } from "@mui/material";
-import Home from "./pages/Home/Home";
+import routes from "./routes";
+import { useRoutes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const allPages = useRoutes(routes);
+
+  // const appTheme =
+
+  const toasterOptions = {
+    style: {
+      fontWeight: 500,
+      fontFamily: "'Montserrat', sans-serif",
+    },
+  };
   return (
-    <StyledEngineProvider>
-      <ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={appTheme}>
         <CssBaseline />
+        <Toaster toastOptions={toasterOptions} />
+        {allPages}
       </ThemeProvider>
     </StyledEngineProvider>
   );
