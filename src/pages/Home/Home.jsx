1349a6bb15560f3409092c9377f06e87535bab89
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-13 21:38:32 +0300

    ref

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,7 @@ import Typography from "@mui/material/Typography";
 import React from "react";
 
 const Home = () => {
-  return (
-    <Box>
-      <Typography variant="h1" color="primary.main">
-        This is Home Component
-      </Typography>
-      <Typography color="text.primary" mb="3rem">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-        laboriosam
-      </Typography>
-
-      <Box my={3}>
-        <Typography variant="h4">Change Theme</Typography>
-      </Box>
-    </Box>
-  );
+  return <Box></Box>;
 };
 
 export default Home;
