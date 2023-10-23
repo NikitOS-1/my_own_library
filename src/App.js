b48b79abc48649474ec05bf84b3b5e810e5b399b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-23 19:02:40 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import {
 } from "@mui/material";
 
 import { appTheme } from "theme";
-import Header from "container/Header/Header";
-import Main from "container/Main/Main";
-import Footer from "container/Footer/Footer";
+// import Header from "container/Header/Header";
+// import Main from "container/Main/Main";
+// import Footer from "container/Footer/Footer";
 import routes from "routes";
 import { useRoutes } from "react-router-dom";
 
@@ -19,9 +19,6 @@ function App() {
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={appsTheme}>
         <CssBaseline />
-        {/* <Header />
-        <Main />
-        <Footer /> */}
         {allPages}
       </ThemeProvider>
     </StyledEngineProvider>
