3cd27d6113af6b1c19168cb01fd4babb4f418fe3
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-10-23 19:40:30 +0300

    compl test redux

--- src/container/Main/Main.jsx
+++ src/container/Main/Main.jsx
@@ -1,20 +1,10 @@
 import { useRoutes } from "react-router-dom";
 import routes from "../../routes";
-import { useDispatch, useSelector } from "react-redux";
-import { inc } from "redux/someReducer";
 
 const Main = () => {
   const allPages = useRoutes(routes);
-  const dispatch = useDispatch();
-  const value = useSelector((state) => state.some.value);
-  console.log(value);
 
-  return (
-    <main>
-      <button onClick={() => dispatch(inc())}>+</button>
-      {allPages}
-    </main>
-  );
+  return <main>{allPages}</main>;
 };
 
 export default Main;
